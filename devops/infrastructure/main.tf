c84338714e2631dd94e9748bf7c4ddb860078d38
jared <magrathj@tcd.ie>
2022-09-24 10:54:49 +0100

    added storage container to terraform script

--- devops/infrastructure/main.tf
+++ devops/infrastructure/main.tf
@@ -33,41 +33,47 @@ resource "azurerm_postgresql_server" "main" {
   ssl_minimal_tls_version_enforced = "TLS1_2"
 }
 
-# resource "azurerm_storage_account" "main" {
-#   name                     = "${var.storage_acct_name}storageacc"
-#   resource_group_name      = azurerm_resource_group.main.name
-#   location                 = azurerm_resource_group.main.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   account_kind             = "StorageV2"
-#   is_hns_enabled           = "true"
-# }
+resource "azurerm_storage_account" "main" {
+  name                     = "${var.storage_acct_name}storageacc"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
 
-# resource "azurerm_storage_data_lake_gen2_filesystem" "main" {
-#   name               = "${var.data_lake_name}storagedatalake"
-#   storage_account_id = azurerm_storage_account.main.id
-# }
+resource "azurerm_storage_data_lake_gen2_filesystem" "main" {
+  name               = "${var.data_lake_name}storagedatalake"
+  storage_account_id = azurerm_storage_account.main.id
+}
 
-# resource "azurerm_synapse_workspace" "main" {
-#   name                                 = "${var.synapse_workspace_name}synapse"
-#   resource_group_name                  = azurerm_resource_group.main.name
-#   location                             = azurerm_resource_group.main.location
-#   storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.main.id
-#   sql_administrator_login              = var.synapse_sql_administrator_login
-#   sql_administrator_login_password     = var.synapse_sql_administrator_password
+resource "azurerm_storage_container" "main" {
+  name                  = "raw"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+resource "azurerm_synapse_workspace" "main" {
+  name                                 = "${var.synapse_workspace_name}synapse"
+  resource_group_name                  = azurerm_resource_group.main.name
+  location                             = azurerm_resource_group.main.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.main.id
+  sql_administrator_login              = var.synapse_sql_administrator_login
+  sql_administrator_login_password     = var.synapse_sql_administrator_password
 
-#   tags = {
-#     Env = "test"
-#   }
-# }
+  identity {
+    type = "SystemAssigned"
+  }
 
-# resource "azurerm_synapse_sql_pool" "main" {
-#   name                 = "${var.synapse_sql_pool_name}synapse"
-#   synapse_workspace_id = azurerm_synapse_workspace.main.id
-#   sku_name             = "DW100c"
-#   create_mode          = "Default"
-# }
+  tags = {
+    Env = "test"
+  }
+}
+
+resource "azurerm_synapse_sql_pool" "main" {
+  name                 = "${var.synapse_sql_pool_name}synapse"
+  synapse_workspace_id = azurerm_synapse_workspace.main.id
+  sku_name             = "DW100c"
+  create_mode          = "Default"
+}
